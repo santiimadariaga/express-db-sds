7eb2519828fb379002860c7a4f45c028ea928f5f
Santiago Madariaga <santiagoamadariaga@gmail.com>
2023-09-05 19:33:14 -0300

    logic merge to front

--- auth/authController.js
+++ auth/authController.js
@@ -17,17 +17,7 @@ export const authController = async (req, res) => {
       }
     );
 
-    // COOKIES OPTIONS
-    // const cookiesOptions = {
-    //   expires: new Date(Date.now() + process.env.COOKIES_TIME_EXPIRES * 1000),
-    //   httpOnly: true,
-    //   secure: true,
-    // };
-    // res.cookie('jwt', token, cookiesOptions).json({ user: user, token: token });
-
-    res.setHeader('JWT-User-Token', token).json({ user: user, token: token });
-    // Se setea el header pero en: '/login'
-    // Ver como setear en: '/'
+    res.status(200).json({ user: user, token: token });
   } catch (error) {
     console.log('Error in controller: ', error);
     res.status(500).json({ message: error.message });
